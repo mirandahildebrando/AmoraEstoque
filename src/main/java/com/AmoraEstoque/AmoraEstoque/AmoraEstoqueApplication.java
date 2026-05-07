54e653f359a47629438c007df58ddd936d771de0
Hildebrando Miranda <mirandahildebrando5@gmail.com>
2026-05-07 17:00:36 -0300

    primeiro commit

---
+++ src/main/java/com/AmoraEstoque/AmoraEstoque/AmoraEstoqueApplication.java
@@ -0,0 +1,13 @@
+package com.AmoraEstoque.AmoraEstoque;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmoraEstoqueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmoraEstoqueApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/AmoraEstoque/AmoraEstoque/AmoraEstoqueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.AmoraEstoque.AmoraEstoque;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmoraEstoqueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
