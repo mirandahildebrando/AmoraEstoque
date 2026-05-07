7f53511ac2adeb3001982da9b183bc99d553ff59
Hildebrando Miranda <mirandahildebrando5@gmail.com>
2026-05-07 17:06:33 -0300

    entidade sale criada

---
+++ src/main/java/com/AmoraEstoque/AmoraEstoque/entity/Sale.java
@@ -0,0 +1,27 @@
+package com.AmoraEstoque.AmoraEstoque.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Sale {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private BigDecimal total;
+
+    private LocalDateTime createdAt;
+
+    @ManyToOne
+    @JoinColumn(name = "company_id")
+    private Company company;
+}
