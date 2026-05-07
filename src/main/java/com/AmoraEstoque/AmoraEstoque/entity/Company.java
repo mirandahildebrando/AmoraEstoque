b629e591b2e39fa5b4e5aaa56cdfc9e371b01537
Hildebrando Miranda <mirandahildebrando5@gmail.com>
2026-05-07 17:03:45 -0300

    entidade company criada

---
+++ src/main/java/com/AmoraEstoque/AmoraEstoque/entity/Company.java
@@ -0,0 +1,20 @@
+package com.AmoraEstoque.AmoraEstoque.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Company {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private Boolean active = true;
+}
