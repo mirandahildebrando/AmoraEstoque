cb0f278c22124245ece91ac5737582dc4073eaab
Hildebrando Miranda <mirandahildebrando5@gmail.com>
2026-05-22 17:39:43 -0300

    fix cors products

--- src/main/java/com/AmoraEstoque/AmoraEstoque/config/CorsConfig.java
+++ src/main/java/com/AmoraEstoque/AmoraEstoque/config/CorsConfig.java
@@ -17,15 +17,25 @@ public class CorsConfig {
             public void addCorsMappings(CorsRegistry registry) {
 
                 registry.addMapping("/**")
+
                         .allowedOrigins(
                                 "http://localhost:3000",
-                                "http://localhost:3001",
-                                "http://localhost:3002",
                                 "https://amora-estoque-frontend.vercel.app"
                         )
-                        .allowedMethods("*")
+
+                        .allowedMethods(
+                                "GET",
+                                "POST",
+                                "PUT",
+                                "DELETE",
+                                "OPTIONS"
+                        )
+
                         .allowedHeaders("*")
-                        .allowCredentials(true);
+
+                        .exposedHeaders("*")
+
+                        .allowCredentials(false);
             }
         };
     }
